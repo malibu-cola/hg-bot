4cad2e1f50724a5ea708ced44696c4cbfb6ebe4d
malibu-cola <ryhrys4269@icloud.com>
2025-05-27 19:22:41 +0900

    feat: kaggleコンペ通知

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ from line_bot.line.send_message import send_line
 from line_bot.nasa_api.fetch_apod import fetch_apod
 from line_bot.nasa_api.new_ws import fetch_new_ws
 from dotenv import load_dotenv
-
+from line_bot.kaggle_notification.fetch_infomation import fetch_competitions
 load_dotenv()
 
 if __name__ == "__main__":
     send_line(fetch_apod())
     if new_ws := fetch_new_ws():
         send_line(new_ws)
+    if kaggle_info := fetch_competitions():
+        send_line(kaggle_info)
---
+++ src/line_bot/kaggle_notification/fetch_infomation.py
@@ -0,0 +1,17 @@
+from kaggle.api.kaggle_api_extended import KaggleApi
+from line_bot.util.translate import translate_text
+
+api = KaggleApi()
+api.authenticate()
+
+def fetch_competitions():
+    ret = ""
+    competitions = api.competitions_list(sort_by='latestDeadline')
+    for comp in competitions:
+        text = comp.description.strip()
+        ret += f"""
+        コンペ: {comp.title}
+        締め切り: {comp.deadline}
+        URL: {comp.ref}
+        {translate_text(text, 'ja')[:100]}...
+        ---"""
